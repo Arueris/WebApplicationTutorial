6b51ae45d351c1e9ab6bf04213d73228b49b3c4e
istbjseve <bjoern.severitt@stud.h-da.de>
2020-02-12 14:01:37 +0100

    add tutorial

---
+++ flaskr/__init__.py
@@ -0,0 +1,45 @@
+import os
+
+from flask import Flask
+
+
+def create_app(test_config=None):
+
+    app = Flask(__name__, instance_relative_config=True)
+
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite')
+    )
+
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(test_config)
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+
+    from flaskr import db
+    db.init_app(app)
+
+    from flaskr import auth
+    app.register_blueprint(auth.bp)
+
+    from flaskr import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
+
+    from flaskr import plot
+    app.register_blueprint(plot.bp)
+
+    return app
+
+
+# actually: https://flask.palletsprojects.com/en/1.1.x/tutorial/views/
